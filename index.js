1c3681ef042e6100057b8c38ab585e4c53427850
renatosans <renato@datacopy.com.br>
2021-10-22 11:47:47 -0300

    Commit inicial de arquivos

---
+++ index.js
@@ -0,0 +1,63 @@
+import mysql from "mysql2";
+import express from "express";
+
+
+var app = express();
+const port = 3000;
+
+
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'h8'
+});
+
+// initial connection to MySQL Database
+mysqlConnection.connect(
+    err => {
+        if (!err) {
+            console.log("DB connection succeeded!");
+        } else {
+            console.log("DB connection failed!\n Error: " + JSON.stringify(err, undefined, 2));
+        }
+    }
+);
+
+// configuring port for Express server
+app.listen(port, () => console.log('Express server is running at port ' + port));
+
+
+
+// recupera o cadastro do jogador
+app.get('/recuperarJogador/:code', (req, res) => {
+    mysqlConnection.query('SELECT * FROM jogador WHERE id = ?', [req.params.code], (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log("Error: " + JSON.stringify(err, undefined, 2));
+        }
+    })
+});
+
+// lista todos os jogadores
+app.get('/listarJogadores', (req, res) => {
+    mysqlConnection.query('SELECT * FROM jogador', (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log("Error: " + JSON.stringify(err, undefined, 2));
+        }
+    })
+});
+
+// lista todos os clubes
+app.get('/listarClubes', (req, res) => {
+    mysqlConnection.query('SELECT * FROM clube', (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log("Error: " + JSON.stringify(err, undefined, 2));
+        }
+    })
+});
