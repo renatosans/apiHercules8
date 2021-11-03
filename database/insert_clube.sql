a795ca254fedb5a09490206b5eb0c57b65836ecb
renatosans <renato@datacopy.com.br>
2021-11-03 10:18:40 -0300

    Inclusão dos scripts do banco de dados

---
+++ database/insert_clube.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `h8`.`clube` (`id`, `pais`, `nome`, `email`, `telefone`) VALUES ('1', 'Brasil', 'Botafogo', 'botafogo@gmail.com.br', '997160011');
+INSERT INTO `h8`.`clube` (`id`, `pais`, `nome`, `email`, `telefone`) VALUES ('2', 'Brasil', 'Corinthians', 'timao@gmail.com.br', '32396699');
+INSERT INTO `h8`.`clube` (`id`, `pais`, `nome`, `email`, `telefone`) VALUES ('3', 'Brasil', 'Santos', 'santos@terra.com.br', '997450022');
---
+++ database/insert_jogador.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `h8`.`jogador` (`id`, `nome`, `email`) VALUES ('1', 'Lionel Messi', 'messi@barcelona.com');
+INSERT INTO `h8`.`jogador` (`id`, `nome`, `email`) VALUES ('2', 'Cristiano Ronaldo', 'r7@realmadri.com');
+INSERT INTO `h8`.`jogador` (`id`, `nome`, `email`) VALUES ('3', 'Robert Lewandowski', 'lewandowski@bayern.com');
+INSERT INTO `h8`.`jogador` (`id`, `nome`, `email`) VALUES ('4', 'Kylian Mbappé', 'kylian@psg.com');
+INSERT INTO `h8`.`jogador` (`id`, `nome`, `email`) VALUES ('5', 'Neymar Junior', 'neymar@psg.com');
