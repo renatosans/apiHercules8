674de7b04f0804a1e45a881a7f67adbf77296b67
renato_sans <renato@datacopy.com.br>
2022-07-16 15:07:54 -0300

    Ajustes

--- database/insert_jogador.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO `h8`.`jogador` (`id`, `nome`, `email`) VALUES ('1', 'Lionel Messi', 'messi@barcelona.com');
-INSERT INTO `h8`.`jogador` (`id`, `nome`, `email`) VALUES ('2', 'Cristiano Ronaldo', 'r7@realmadri.com');
-INSERT INTO `h8`.`jogador` (`id`, `nome`, `email`) VALUES ('3', 'Robert Lewandowski', 'lewandowski@bayern.com');
-INSERT INTO `h8`.`jogador` (`id`, `nome`, `email`) VALUES ('4', 'Kylian Mbappé', 'kylian@psg.com');
-INSERT INTO `h8`.`jogador` (`id`, `nome`, `email`) VALUES ('5', 'Neymar Junior', 'neymar@psg.com');
