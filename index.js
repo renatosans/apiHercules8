1b5b8355d359656cedea27e403e8451b22e84b6b
renato_sans <renato@datacopy.com.br>
2022-07-16 09:44:02 -0300

    Ajustes

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-
 import express from 'express';
 import { PrismaClient } from '@prisma/client'
 
 
-var app = express();
 const port = 3000;
-
+const app = express();
 const prisma = new PrismaClient();
 
 
 // inicia a API escutando na porta 3000
 app.listen(port, () => console.log('Express escutando chamadas na porta ' + port));
 
-
 // recupera o cadastro do jogador
 app.get('/recuperarJogador/:id', (req, res) => {
-    mysqlConnection.query('SELECT * FROM jogador WHERE id = ?', [req.params.id], (err, rows, fields) => {
-        if (!err) {
-            res.send(rows);
-        } else {
-            console.log("Error: " + JSON.stringify(err, undefined, 2));
-        }
-    })
+    prisma.jogador.findUnique({ where: { id: Number(req.params.id) }, select: { nome: true, email: true }})
+    .then((jogadores) => res.send(jogadores))
+    .catch((error) => console.log("Error: " + error.message))
 })
 
 // lista todos os jogadores
 app.get('/listarJogadores', (req, res) => {
-    // prisma.jogador.findMany()
-    prisma.jogador.findUnique({ where: { id: 4 }, select: { nome: true, email: true }})
+    prisma.jogador.findMany()
         .then((jogadores) => res.send(jogadores))
-        .catch((error) => console.log("Error: " + JSON.stringify(error))  )
+        .catch((error) => console.log("Error: " + error.message))
 })
