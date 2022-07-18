8ee4aad9d8662d0c1ce0f0025c0dbc32fb6fe7e2
renato_sans <renato@datacopy.com.br>
2022-07-18 08:07:20 -0300

    Ajustes

--- index.js
+++ index.js
@@ -17,26 +17,26 @@ app.listen(port, () => console.log('Express escutando chamadas na porta ' + port
 app.get('/listarJogadores', (req, res) => {
     prisma.jogador.findMany()
         .then((jogadores) => res.send(jogadores))
-        .catch((error) => console.log("Error: " + error.message))
+        .catch((error) => res.send("Error: " + error.message))
 })
 
 // recupera o cadastro do jogador
 app.get('/recuperarJogador/:id', (req, res) => {
     prisma.jogador.findUnique({ where: { id: Number(req.params.id) } })
     .then((jogador) => res.send(jogador))
-    .catch((error) => console.log("Error: " + error.message))
+    .catch((error) => res.send("Error: " + error.message))
 })
 
 // lista todos os clubes
 app.get('/listarClubes', (req, res) => {
     prisma.clube.findMany()
     .then((clubes) => res.send(clubes))
-    .catch((error) => console.log("Error: " + error.message))
+    .catch((error) => res.send("Error: " + error.message))
 })
 
 // recupera o cadastro do clube
 app.get('/recuperarClube/:id', (req, res) => {
     prisma.clube.findUnique({ where: { id: Number(req.params.id) }, include: { jogadores: true } })
     .then((clube) => res.send(clube))
-    .catch((error) => console.log("Error: " + error.message))
+    .catch((error) => res.send("Error: " + error.message))
 })
