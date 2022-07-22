a7b560c8437e9dadb08686aa1f61b49943aeea5e
renato_sans <renato@datacopy.com.br>
2022-07-22 11:06:16 -0300

    Ajustes

--- api/clubes/[id].js
+++ api/clubes/[id].js
@@ -1,34 +1,39 @@
 const prisma = require('../../config/db');
 
 
-module.exports = (req, res) => {
-	switch (req.method) {
-		case "GET": {
-			return getClube(req, res);
-		}
-		case "DELETE": {
-			return deleteClube(req, res);
-		}
-		case "PUT": {
-			return updateClube(req, res);
-		}
-	}
-}
-
 module.exports = {
+	default: (req, res) => {
+		const { id } = req.query;
+
+		switch (req.method) {
+			case "GET": {
+				res.send(`Registro ${id} recuperado`)
+			}
+			case "DELETE": {
+				res.send(`Registro ${id} excluido com sucesso`)
+			}
+			case "PUT": {
+				res.send(`Registro ${id} atualizado com sucesso`)
+			}
+		}
+	},
     get: (req, res) => { 
-		const { id } = req.params;
+		const { id } = req.query;
 
 		prisma.clube.findUnique({ where: { id: Number(id) }, include: { jogadores: true } })
 		.then((clube) => res.send(clube))
 		.catch((error) => res.send("Error: " + error.message))	
     },
     del: (req, res) => {
+		const { id } = req.query;
+
 		// TODO : delete
-		res.send('Registro excluido com sucesso');
+		res.send(`Registro ${id} excluido no BD`)
     },
     put: (req, res) => {
+		const { id } = req.query;
+
 		// TODO : update
-		res.send('Registro atualizado com sucesso');
+		res.send(`Registro ${id} atualizado no BD`)
     },
 }
