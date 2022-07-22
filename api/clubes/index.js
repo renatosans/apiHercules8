dc811bb2b82024035837166414a7347fdb31a454
renato_sans <renato@datacopy.com.br>
2022-07-22 10:26:07 -0300

    Ajustes

--- api/clubes/index.js
+++ api/clubes/index.js
@@ -1,25 +1,24 @@
 const prisma = require('../../config/db');
 
 
-module.exports = (req, res) => {
-	switch (req.method) {
-		case "POST": {
-			return postClube(req, res);
-		}
-		case "GET": {
-			return getClubes(req, res);
-		}
-	}
-}
-
 module.exports = {
+	default: (req, res) => {
+		switch (req.method) {
+			case "POST": {
+				res.send('Clube salvo com sucesso')
+			}
+			case "GET": {
+				res.send('Lista de clubes')
+			}
+		}
+	},
     get: (req, res) => { 
 		prisma.clube.findMany()
 		.then((clubes) => res.send(clubes))
-		.catch((error) => res.send("Error: " + error.message))			
+		.catch((error) => res.send("Error: " + error.message))
     },
     post: (req, res) => {
 		// TODO: insert
-		res.send('Clube salvo com sucesso');        
+		res.send('Clube salvo com sucesso')
     }
 }
